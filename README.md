950b8de0c38e02e274e8acfd8ddd6081e9beeb5e
SrBaliardo <tao.john007@icloud.com>
2025-01-23 15:22:09 -0300

    fix(code): update README.

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
+# SIM, é outro famoso Pokedèx!
+
+## É legal e é impossível não ter um se você cresceu com ele. Sem contar que um dos primeiros códigos com consumo de API que fiz (rs)
+
+<p>Experimente o código e o tratamento.
+Este usa o consumo de API para exibir informações e imagens dos infames pokemons.</p>
+
+"Temos que pegaaaar ! Pokemon !" XD
+
+<br>
+
 # YES, it's another famous Pokedèx!
-## It's cool and it's impossible not to have one if you grew up with it.
+
+## It's cool and it's impossible not to have one if you grew up with it. Not to mention that it was one of the first API consumption codes I made (lol)
 
 <p>Give the code and treatment a try.
 This one uses API consumption to display information and images of the infamous pokemons.</p>
+
+We have to take it! Pokemon ! lol
 
 <br>
 
@@ -11,6 +25,10 @@ https://srbaliardo.github.io/pokedex
 
 <br>
 
-" <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"> | 
-  <img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"> | 
-  <img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"> "
+<img height="auto" width="500" src="/images/animation_pokedex.gif">
+
+<br>
+
+" <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"> |
+<img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"> |
+<img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"> "
--- script.js
+++ script.js
@@ -1,56 +1,100 @@
-const pokemonName = document.querySelector('.pokemon-name');
-const pokemonId = document.querySelector('.pokemon-id');
-const pokemonImg = document.querySelector('.pokemon-img');
-const form = document.querySelector('.form');
-const inputSearch = document.querySelector('.input-search');
-const btnPrev = document.querySelector('.btn-prev');
-const btnNext = document.querySelector('.btn-next');
+const pokemonName = document.querySelector(".pokemon-name");
+const pokemonId = document.querySelector(".pokemon-id");
+const pokemonImg = document.querySelector(".pokemon-img");
+const form = document.querySelector(".form");
+const inputSearch = document.querySelector(".input-search");
+const pokemonDescription = document.querySelector(".description");
+const pokemonTypes = document.querySelector(".types");
+const pokemonSkills = document.querySelector(".skills");
+const pokemonHeight = document.querySelector(".height");
+const pokemonWeight = document.querySelector(".weight");
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
 
 let search = 1;
 
-const fetchPokemon  = async (pokemon) => {
-    const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-
-    if (apiResponse.status == 200){
-        const data = await apiResponse.json();
-        return data;
+const fetchPokemon = async (pokemon) => {
+  try {
+    const apiResponse = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    );
+    if (apiResponse.status === 200) {
+      const data = await apiResponse.json();
+      return data;
     }
-}
+  } catch (error) {
+    console.error("Erro ao buscar Pokémon:", error);
+  }
+};
+
+const fetchPokemonDescription = async (pokemonId) => {
+  try {
+    const apiResponse = await fetch(
+      `https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`
+    );
+    if (apiResponse.status === 200) {
+      const data = await apiResponse.json();
+      const englishEntry = data.flavor_text_entries.find(
+        (entry) => entry.language.name === "en"
+      );
+      return englishEntry
+        ? englishEntry.flavor_text.replace(/\n|\f/g, " ")
+        : "No description available.";
+    }
+  } catch (error) {
+    console.error("Erro ao buscar descrição do Pokémon:", error);
+  }
+};
 
 const renderPokemon = async (pokemon) => {
+  pokemonName.innerHTML = "Loading ... ";
+  pokemonImg.src = "images/bubble-pixel-dots.png";
 
-    pokemonName.innerHTML = 'Loading ... ';
-    pokemonImg.src = 'images/bubble-pixel-dots.png';
+  const data = await fetchPokemon(pokemon);
 
-    const data = await fetchPokemon(pokemon);
+  if (data) {
+    const description = await fetchPokemonDescription(data.id);
 
-    if (data) {
-        pokemonId.innerHTML = data.id + '-';
-        pokemonName.innerHTML = data.name;
-        pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-        inputSearch.value = '';
-        search = data.id;
-    } else {
-        pokemonId.innerHTML = '¬¬';
-        pokemonName.innerHTML = ' Not Found';
-        pokemonImg.src = 'images/bubble-pixel-sad.png';
-        inputSearch.value = '';   
-    }
-}
+    pokemonId.innerHTML = "#" + data.id;
+    pokemonName.innerHTML = data.name;
+    pokemonImg.src = data["sprites"]["front_default"];
+    pokemonDescription.innerHTML = description;
+    pokemonTypes.innerHTML = data.types
+      .map((typeInfo) => typeInfo.type.name)
+      .join(", ");
+    pokemonSkills.innerHTML = data.abilities
+      .map((abilityInfo) => abilityInfo.ability.name)
+      .join(", ");
+    pokemonHeight.innerHTML = data.height / 10;
+    pokemonWeight.innerHTML = data.weight / 10;
+    inputSearch.value = "";
+    search = data.id;
+  } else {
+    pokemonId.innerHTML = "¬¬";
+    pokemonName.innerHTML = " Not Found";
+    pokemonImg.src = "images/bubble-pixel-sad.png";
+    pokemonDescription.innerHTML = "---";
+    pokemonTypes.innerHTML = "---";
+    pokemonSkills.innerHTML = "---";
+    pokemonHeight.innerHTML = "0.00";
+    pokemonWeight.innerHTML = "0.00";
+    inputSearch.value = "";
+  }
+};
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    renderPokemon(inputSearch.value.toLowerCase());
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  renderPokemon(inputSearch.value.toLowerCase());
 });
 
-btnPrev.addEventListener('click', () =>{
-    if (search > 1) {
-        search -= 1;
-        renderPokemon(search);
-    }
-});
-
-btnNext.addEventListener('click', () =>{
-    search += 1;
+btnPrev.addEventListener("click", () => {
+  if (search > 1) {
+    search -= 1;
     renderPokemon(search);
+  }
+});
+
+btnNext.addEventListener("click", () => {
+  search += 1;
+  renderPokemon(search);
 });
